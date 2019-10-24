c6f32b8120fc2cbeed4a6fdd3ab3b4a4aa1c5f4e
SMITC648 <christopher.y.smith.-nd@disney.com>
2019-10-24 08:57:43 -0400

    Start WebAudio with user action

--- itsgonnarain/itsgonnarain.js
+++ itsgonnarain/itsgonnarain.js
@@ -3,6 +3,7 @@ const loopStart = 4.0
 const loopEnd = 4.8
 const rate = 1.002
 const audioContext = new AudioContext()
+let audioBuffer;
 
 const startLoop = (audioBuffer, pan = 0, rate = 1) => {
   const sourceNode = audioContext.createBufferSource()
@@ -29,14 +30,18 @@ const startAnimation = () => {
   element.style.animation = `spin ${cycleLength}s linear infinite`
 }
 
+const start = (audioBuffer) => {
+  document.querySelector('.message').remove()
+  startLoop(audioBuffer, -1)
+  startLoop(audioBuffer, 1, rate)
+  startAnimation()
+}
+
 fetch('0001.mp3')
   .then(response => response.arrayBuffer())
   .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
   .then(audioBuffer => {
-    startLoop(audioBuffer, -1)
-    startLoop(audioBuffer, 1, rate)
-
-    startAnimation()
+    document.querySelector('.message').innerText = 'Touch anywhere to begin.'
+    document.querySelector('body').onclick = () => start(audioBuffer)
   })
   .catch(e => console.log(e))
-
