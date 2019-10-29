1b842daf5fb19e9846bab9ea9bcc75b67878f7e5
SMITC648 <christopher.y.smith.-nd@disney.com>
2019-10-29 13:51:29 -0400

    Add EQ (sound not starting sometimes...?)

--- discreetmusic/discreetmusic.js
+++ discreetmusic/discreetmusic.js
@@ -1,62 +1,81 @@
 const $ = document.querySelector.bind(document)
 
-const startItUp = () => {
-  const makeSynth = () => {
-    const envelope = {
-      attack: 0.1,
-      release: 2,
-      releaseCurve: 'linear'
-    }
-    const filterEnvelope = {
-      baseFrequency: 200,
-      octaves: 3,
-      attack: 0,
-      decay: 0,
-      release: 9999
-    }
-    return new Tone.DuoSynth({
-      volume: -20,
-      harmonicity: 1,
-      vibratoRate: 0.1,
-      vibratoAmount: 0.2,
-      voice0: {
-        oscillator: {type: 'sawtooth'},
-        envelope,
-        filterEnvelope
-      },
-      voice1: {
-        oscillator: {type: 'sine'},
-        envelope,
-        filterEnvelope
-      }
-    })
+const makeSynth = () => {
+  const envelope = {
+    attack: 0.1,
+    release: 4,
+    releaseCurve: 'linear'
   }
+  const filterEnvelope = {
+    baseFrequency: 200,
+    octaves: 3,
+    release: 9999
+  }
+  return new Tone.DuoSynth({
+    volume: -20,
+    harmonicity: 1,
+    vibratoRate: 0.1,
+    vibratoAmount: 0.1,
+    voice0: {
+      oscillator: {type: 'sawtooth'},
+      envelope,
+      filterEnvelope
+    },
+    voice1: {
+      oscillator: {type: 'sine'},
+      envelope,
+      filterEnvelope
+    }
+  })
+}
+
+const leftSynth = makeSynth()
+const rightSynth = makeSynth()
   
-  const leftSynth = makeSynth()
-  const rightSynth = makeSynth()
-  
-  leftSynth.vibratoRate.value = 20
-  
-  const leftPanner = new Tone.Panner(-0.5)
-  const rightPanner = new Tone.Panner(0.5)
-  
-  const echo = new Tone.FeedbackDelay('4n', 0.4)
-  const delay = Tone.context.createDelay(6.0)
-  const delayFade = Tone.context.createGain()
-  
-  delay.delayTime.value = 6.0
-  delayFade.gain.value = 0.75
-  
-  leftSynth.connect(leftPanner)
-  rightSynth.connect(rightPanner)
-  leftPanner.connect(echo)
-  rightPanner.connect(echo)
-  echo.toMaster()
-  echo.connect(delay)
-  delay.connect(Tone.context.destination)
-  delay.connect(delayFade)
-  delayFade.connect(delay)
-  
+const leftPanner = new Tone.Panner(-0.5)
+const rightPanner = new Tone.Panner(0.5)
+
+const EQUALIZER_CENTER_FREQUENCIES = [
+  100, 125, 160, 200, 250, 315, 400, 500, 630, 800, 1000, 1250,
+  1600, 2000, 2500, 3150, 4000, 5000, 6300, 8000, 10000
+]
+
+const equalizer = EQUALIZER_CENTER_FREQUENCIES.map(frequency => {
+  const filter = Tone.context.createBiquadFilter()
+  filter.type = 'peaking'
+  filter.frequency.value = frequency
+  filter.Q.value = 4.31
+  filter.gain.value = 0
+  return filter
+})
+
+const echo = new Tone.FeedbackDelay('4n', 0.4)
+const delay = Tone.context.createDelay(6.0)
+const delayFade = Tone.context.createGain()
+delay.delayTime.value = 6.0
+delayFade.gain.value = 0.75
+
+leftSynth.connect(leftPanner)
+rightSynth.connect(rightPanner)
+
+leftPanner.connect(equalizer[0])
+rightPanner.connect(equalizer[0])
+
+equalizer.forEach((eqBand, index) => {
+  if (index < equalizer.length - 1) {
+    eqBand.connect(equalizer[index + 1])
+  } else {
+    eqBand.connect(echo)
+  }
+})
+
+echo.toMaster()
+echo.connect(delay)
+delay.connect(Tone.context.destination)
+delay.connect(delayFade)
+delayFade.connect(delay)
+
+const startItUp = () => {
   new Tone.Loop(time => {
     leftSynth.triggerAttackRelease('C5', '1:2', time)
     leftSynth.setNote('D5', '+0:2')
@@ -79,11 +98,42 @@ const startItUp = () => {
    Tone.Transport.start()
 }
 
-
-
 const startButton = $('#start-button')
 startButton.onclick = () => {
   console.log('Starting')
   startItUp()
+  console.log(leftSynth)
+  console.log(rightSynth)
   startButton.disabled = true
 }
+
+const initEqualizerUI = (container, equalizer) => {
+  console.log(container)
+  equalizer.forEach(equalizerBand => {
+    const frequency = equalizerBand.frequency.value
+
+    const wrapper = document.createElement('div')
+    const slider = document.createElement('input')
+    slider.type = 'range'
+    slider.min = -12
+    slider.max = 12
+    slider.value = 0
+    slider.step = 0.1
+    slider.onchange = event => {
+      let gain = +event.target.value
+      console.log(gain)
+      equalizerBand.gain.value = gain
+    }
+    const label = document.createElement('label')
+    label.innerText = frequency >= 1000 ? `${frequency / 1000}K` : frequency
+
+    wrapper.classList.add('slider-wrapper')
+    slider.classList.add('slider')
+
+    wrapper.appendChild(slider)
+    wrapper.appendChild(label)
+    container.appendChild(wrapper)
+  })
+}
+
+initEqualizerUI($('.eq'), equalizer)
